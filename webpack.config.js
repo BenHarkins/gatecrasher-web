2a27375ef4991c37474bb8a946816674adb163a0
Ben Harkins <ben@benharkins.com>
2015-10-24 15:49:44 -0500

    Initial demo check-in

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+var Webpack = require('Webpack')
+var path = require('path')
+var nodeModulesPath = path.resolve(__dirname, 'node_modules')
+var buildPath = path.resolve(__dirname, 'public', 'build')
+var mainPath = path.resolve(__dirname, 'app', 'main.js')
+
+var config = {
+	devTool: 'eval',
+	entry: [
+		'webpack/hot/dev-server',
+		'webpack-dev-server/client?http://localhost:8080',
+		path.resolve(nodeModulesPath, 'mithril/mithril.min.js'),
+		mainPath
+	],
+	output: {
+		path: buildPath,
+		filename: 'bundle.js',
+		publicPath: '/build/'
+	},
+	module: {
+		loaders: [
+			// {
+			// 	test: /\.js$/,
+			// 	loader: 'babel',				
+			// 	exclude: [nodeModulesPath]
+			// },
+ 			{ 
+ 				test: /\.css$/, 
+ 				loader: "style!css"
+ 			}			
+		]
+	},
+	plugins: [new Webpack.HotModuleReplacementPlugin()]
+};
+
+module.exports = config;
+
+// module.exports = {
+// 	entry: "./entry.js",
+// 	output: {
+// 		path: __dirname,
+// 		filename: "bundle.js"
+// 	},
+// 	module: {
+// 		loaders : [
+// 			{ test: /\.css$/, loader: "style!css"}
+// 		]
+// 	}
+// }
